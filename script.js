b632661981b80325893f1ada8986b7864d5cbf03
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-26 01:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+function form(name, age){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+		if(age>=18){
+			resolve(`Welcome, ${name} You can vote.`)
+		}
+		else{
+			reject(`Oh sorry ${name} You aren't old enough.`)
+		}
+			},4000)
+	})
+}
+
+function validation(event){
+event.preventDefault()
+const name=document.getElementById("name").value
+const age=document.getElementById("age").value
+	
+	form(name,age)
+	.then((alert1)=>{
+		alert(alert1)
+	}) 
+		.catch((alert1)=>{
+			alert(alert1)
+		})
+}
